2600896cd09a7043455a540d456070865de0bdd3
Richard Willis <willis.rh@gmail.com>
2010-05-24 00:32:01 +0100

    tree: icons and better events

--- js/jquery.ui.tree.js
+++ js/jquery.ui.tree.js
@@ -12,31 +12,88 @@
 	$.widget('ui.tree', {
 
 		options : {
-			width: 320
+			width: 320,
+			icons: {
+				'listopen': 'ui-icon-triangle-1-s',
+				'listclosed': 'ui-icon-triangle-1-e'
+			}
 		},
 		
 		_create : function(){
 
-			this.element.width( this.options.width ).addClass( 'ui-helper-reset ui-tree ui-widget ui-widget-content ui-corner ui-corner-all' );
+			var self = this, options = this.options;
 
-			this.element.find('li').addClass('ui-helper-clearfix');
+			this.element
+				.width( this.options.width )
+				.addClass( 'ui-helper-reset ui-tree ui-widget ui-widget-content ui-corner ui-corner-all' )
+				.find( 'li' )
+					.addClass( 'ui-helper-clearfix' )
+					.end()
+				.find( 'a' ).each(function(){
 
-			this.element.find('a').hover(
-				function(){
-					$(this).addClass('ui-state-hover ui-corner-all');
-				},
-				function(){
-					$(this).removeClass('ui-state-hover ui-corner-all');
-				}
-			);
+					var icon, childlist = $(this).next();
+
+					if ( childlist.length && childlist[0].nodeName == 'UL' ){
+
+						icon = childlist.is(':visible') ? options.icons.listopen : icon = options.icons.listclosed;
+					} else {
+
+						icon = 'ui-tree-icon-transparent';
+					}
+
+					$('<span />')
+						.data('childlist', childlist)
+						.addClass( 'ui-tree-hitarea ui-icon ' + icon )
+						.prependTo( this )
+						.bind('toggle', function(){
+
+							var childlist = $( this ).data('childlist');
+
+							$( this )
+								.removeClass( options.icons.listopen + ' ' + options.icons.listclosed )
+								.parent()
+									.next()
+									.toggle();
+
+
+							if ( childlist.length ){
+
+								if ( childlist.is(':visible') ){
+									
+									$( this ).addClass( options.icons.listopen );
+
+									self._trigger('open', null, { list: childlist });
+
+								} else {
+									
+									$( this ).addClass( options.icons.listclosed );
+
+									self._trigger('close', null, { list: childlist });
+								}
+							}
+						})
+						.bind('click', function(){
+
+							$( this ).trigger( 'toggle' );
+						});
+
+					$(this).hover(
+						function(){
+							$( this ).addClass( 'ui-state-hover ui-corner-all' );
+						},
+						function(){
+							$( this ).removeClass( 'ui-state-hover ui-corner-all' );
+						}
+					);
+				});
 		},
 
 		destroy : function(){
 
 			this.element
-				.removeClass('ui-helper-reset ui-tree ui-widget ui-widget-content ui-corner ui-corner-all')
-				.find('li')
-				.removeClass('ui-helper-clearfix');
+				.removeClass( 'ui-helper-reset ui-tree ui-widget ui-widget-content ui-corner ui-corner-all' )
+				.find( 'li' )
+				.removeClass( 'ui-helper-clearfix' );
 
 			$.Widget.prototype.destroy.apply(this, arguments);
 		}
