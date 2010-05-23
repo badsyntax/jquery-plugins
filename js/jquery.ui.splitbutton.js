b769e7edeb4eaa60e38ca4d0978936ae08b07575
Richard Willis <willis.rh@gmail.com>
2010-05-23 23:14:58 +0100

    added callback methods and better events

--- js/jquery.ui.splitbutton.js
+++ js/jquery.ui.splitbutton.js
@@ -10,127 +10,140 @@
  *
  */
 
-(function( $ ) {
+(function($) {
 
-$.widget('ui.splitbutton', {
-	
-	options: {
-		width: 'auto'
-	},
-	
-	_create : function(){
-
-		var self = this;
-
-		this.elements = {
-			button: this.element.find( 'button:eq(0)' ).button(),
-			hitarea: this.element.find( 'button:eq(1)' ).button({
-				text: false,
-				icons: {
-					primary: 'ui-icon-triangle-1-s'
-				}
-			}),
-			menulist: $('<ul />').appendTo('body')
-		};
+	$.widget('ui.splitbutton', {
 		
-		this.element.buttonset();
-
-		this.elements.menulist
-			.addClass('ui-splitbutton-menu ui-widget ui-widget-content ui-corner-all ui-helper-reset')
-			.attr('role', 'listbox')
-			.width( 
-				this.options.width == 'inherit' ? 
-				this.elements.button.width() + this.elements.hitarea.width()  : 
-				this.options.width 
-			);
-
-		this._build();
-
-		this._bind();
-	},
-
-	_build : function(){
-
-		var self = this
-
-		$.each(this.options.items, function(label){
-
-			var callback = this;
-
-			var anchor = $( '<a />' , {
-				href: '#',
-				mousedown: function(){
-
-					callback.apply( this, arguments );
-
-					self.elements.hitarea.blur();
-				}
-			}).hover(
-				function(){
-					$(this).addClass('ui-state-hover');
-				},
-				function(){
-					$(this).removeClass('ui-state-hover');
-				}
-			).addClass('ui-corner-all').html( label );
-
-			$( '<li />' )
-				.attr('role', 'menuitem')
-				.append( anchor )
-				.addClass( 'ui-helper-reset' )
-				.appendTo( self.elements.menulist );
-
-		});
-	},
-	
-	_bind : function(){
-
-		var self = this;
+		options: {
+			width: 'auto'
+		},
 		
-		this.elements.hitarea
-		.bind( 'click', function(){
+		_create : function(){
 
-			$(this).css({ outline: 'none' }).focus();
+			var self = this;
 
-			self.refresh();
+			this.elements = {
+				button: this.element.find( 'button:eq(0)' ).button(),
+				hitarea: this.element.find( 'button:eq(1)' ).button({
+					text: false,
+					icons: {
+						primary: 'ui-icon-triangle-1-s'
+					}
+				}),
+				menulist: $('<ul />').appendTo('body')
+			};
+			
+			this.element.buttonset();
+			
+			this.elements.hitarea
+				.attr('role', 'hitarea')
+				.addClass('ui-splitbutton-hitarea');
 
-			self.open()
-		})
-		.bind( 'blur', function(){
+			this.elements.menulist
+				.addClass('ui-splitbutton-menu ui-widget ui-widget-content ui-corner-all ui-helper-reset')
+				.attr('role', 'listbox')
+				.width( 
+					this.options.width == 'inherit' ? 
+					this.elements.button.width() + this.elements.hitarea.width()  : 
+					this.options.width 
+				);
 
-			self.close();
-		});
-	},
-	
-	refresh : function(){
+			this._build();
+
+			this._bind();
+		},
+
+		_build : function(){
+
+			var self = this
+
+			$.each(this.options.items, function(label){
+
+				var callback = this;
+
+				var anchor = $( '<a />' , {
+					href: '#',
+					mousedown: function(event){
+
+						callback.apply( this, arguments );
+
+						self.elements.hitarea.blur();
+
+						self._trigger('select', event);
+					}
+				}).hover(
+					function(){
+						$(this).addClass('ui-state-hover');
+					},
+					function(){
+						$(this).removeClass('ui-state-hover');
+					}
+				).addClass('ui-corner-all').html( label );
+
+				$( '<li />' )
+					.attr('role', 'menuitem')
+					.append( anchor )
+					.addClass( 'ui-helper-reset' )
+					.appendTo( self.elements.menulist );
+
+			});
+		},
 		
-		var offset = this.elements.hitarea.offset();
+		_bind : function(){
 
-		this.elements.menulist.css({
-			left: ( offset.left - this.elements.menulist.innerWidth() ) + this.elements.hitarea.width(),
-			top: offset.top + this.elements.hitarea.outerHeight(),
-		});
-	},
+			var self = this;
+			
+			this.elements.hitarea
+			.bind( 'click', function(){
+			
+				$(this).focus();
+				
+				self.open()
+			})
+			.bind( 'blur', function(){
 
-	open : function(){
+				self.close();
+			});
+
+			this.elements.menulist
+			.bind('show', function(){
+
+				var offset = self.elements.hitarea.offset();
+
+				self.elements.menulist.css({
+					left: ( offset.left - self.elements.menulist.innerWidth() ) + self.elements.hitarea.width(),
+					top: offset.top + self.elements.hitarea.outerHeight(),
+				});
+			})
+			.bind('hide', function(){
+
+				$(this).css({ left: -9999 });
+			});
+		},
 		
-		this.elements.menulist.show();
-	},
+		open : function(){
+			
+			this.elements.menulist.trigger('show');
 
-	close : function(){
-		
-		this.elements.menulist.hide();
-	},
+			this._trigger('open');
+		},
 
-	destroy : function(){
+		close : function(){
+			
+			this.elements.menulist.trigger('hide');
 
-		this.elements.menulist.remove();
+			this._trigger('close');
+		},
 
-		this.element.buttonset('destroy');
+		destroy : function(){
 
-		$.Widget.prototype.destroy.apply(this, arguments);
-	}
-});
+			this.elements.menulist.remove();
+
+			this.element.buttonset('destroy');
+
+			$.Widget.prototype.destroy.apply(this, arguments);
+		}
+	});
 
 
 })(jQuery);
