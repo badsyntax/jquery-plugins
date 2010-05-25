eb5c00e71b00eab1e4e44bc3384bf2fc141ce252
Richard Willis <willis.rh@gmail.com>
2010-05-25 21:16:56 +0100

    added demo file scripts

---
+++ get-child-list.php
@@ -0,0 +1,13 @@
+<?
+	sleep(2);
+?>
+
+<li><a href="#">Test 1</a></li>
+<li><a href="#">Test 2</a></li>
+<li>
+	<a href="#">Test 3</a>
+	<ul></ul>
+</li>
+<li><a href="#">Test 4</a></li>
+<li><a href="#">Test 5</a></li>
+<li><a href="#">Test 6</a></li>
