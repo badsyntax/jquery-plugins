48a0ec6a68d651b325c3a4a447355790be1d9b70
Richard Willis <willis.rh@gmail.com>
2010-05-29 12:53:21 +0100

    scrollbar: fixed scrolling ratio values

--- scrollbar/js/jquery.ui.scrollbar.js
+++ scrollbar/js/jquery.ui.scrollbar.js
@@ -36,27 +36,29 @@
 					.addClass( 'ui-scrollbar-content-container' )
 				,
 				baseContainer: $('<div />')
-					.addClass( 'ui-scrollbar-base-container ui-widget-content ui-corner-all' )
+					.addClass( 'ui-widget-content ui-corner-all ui-scrollbar-base-container' )
 				,
 				base: $('<div />')
 					.addClass( 'ui-scrollbar-base' )
 				,
 				face: $('<a />')
 					.attr('href', '#')
-					.addClass( 'ui-scrollbar-face ui-state-default ui-widget-content ui-corner-all' )
+					.addClass( 'ui-state-default ui-widget-content ui-corner-all ui-scrollbar-face' )
 				,
 				arrowUpContainer: $('<div />')
-					.addClass( 'ui-scrollbar-arrow-up ui-state-default ui-corner-all' )
+					.addClass( 'ui-state-default ui-corner-all ui-scrollbar-arrow-up' )
 				,
 				arrowUp: $('<div />')
 					.addClass( 'ui-icon ui-icon-triangle-1-n')
 				,
 				arrowDownContainer: $('<div />')
-					.addClass( 'ui-scrollbar-arrow-down ui-state-default ui-corner-all' )
+					.addClass( 'ui-state-default ui-corner-all ui-scrollbar-arrow-down' )
 				,
 				arrowDown: $('<div />')
 					.addClass( 'ui-icon ui-icon-triangle-1-s' )
 			};
+
+			// need to check if scrollheight > height and scrollwidth > width
 
 			this.elements.contentContainer.append( this.element.contents() ).prependTo( this.element );
 			
@@ -77,35 +79,42 @@
 
 		_resize : function(){
 			
-			
-			this.elements.baseContainer.height( this.element.height() - 4 );
-
-			this.elements.base.height( this.element.height() - 40 );
-
 			var ratio = Math.round( ( this.element.height() / this.element[0].scrollHeight ) * 100 );
 
-			var height = ( this.elements.base.height() / 100 ) * ratio;
+			this._data = {
+				elementHeight: this.element.height(),
+				contentContainerHeight: this.elements.contentContainer.height(),
+				baseContainerHeight: this.element.height() - 4,
+				baseHeight: this.element.height() - 40,
+				faceHeight: ( ( this.element.height() - 40 ) / 100 ) * ratio 
+			};
 
-			this.elements.face.height( height );
+			this.elements.baseContainer.height( this._data.baseContainerHeight );
+
+			this.elements.base.height( this._data.baseHeight );
+
+			this.elements.face.height( this._data.faceHeight );
 		},
 
 		_bind : function(){
 
 			var self = this;
 
-			function hover( event ){
+			function hoverClick( event ){
 
-				if (event.type == 'mousedown' && event.target === self.elements.arrowDown[0] ) {
+				if (event.type == 'mousedown' && ( event.target === self.elements.arrowDown[0] || event.target == self.elements.arrowUp[0] ) ) {
 
-					self._scroll( event, false, -1 );
+					self._scroll( event );
 				}
 
 				$( this ).toggleClass( 'ui-state-hover ui-scrollbar-state-hover' );
 			}
 
-			this.elements.arrowUpContainer.bind('mouseenter mouseleave mousedown mouseup', hover );
+			this.elements.arrowUpContainer
+				.bind('mouseenter mouseleave mousedown mouseup', hoverClick );
 			
-			this.elements.arrowDownContainer.bind('mouseenter mouseleave mousedown mouseup', hover );
+			this.elements.arrowDownContainer
+				.bind('mouseenter mouseleave mousedown mouseup', hoverClick );
 
 			this.elements.face
 			.bind('mouseenter mouseleave', function(){
@@ -125,20 +134,35 @@
 			});
 
 			this.element.bind('drag', function(event, ui) {
-				self._scroll(event, -ui.position.top);
+
+				self._scroll(event, ui.position.top );
 			});
 		},
 
-		_scroll: function( event, val, amount ){
+		_scroll: function( event, val ){
 
-			if ( !val ) {
+			var marginTop = parseInt( this.elements.contentContainer.css('marginTop').replace(/px$/, '') );
 
-				var marginTop = parseInt( this.elements.contentContainer.css('marginTop').replace(/px$/, '') );
-
-				val = marginTop + amount;
+			if ( event.target == this.elements.arrowDown[0] ) {
+				
+				val = marginTop - 2;
+				
+			}
+			if ( event.target == this.elements.arrowUp[0] ) {
+				
+				val = marginTop + 2;
 			}
 
-			this.elements.contentContainer.css({ marginTop:  val + 'px' });
+
+			var 
+				ratio = ( val / ( this._data.baseHeight - this._data.faceHeight ) ) * 100,
+				height = Math.round( ( ( this._data.contentContainerHeight - this._data.elementHeight ) / 100 ) * ratio ) + 2;
+
+			this.elements.face.css( { 'top': -val } );
+
+			this.elements.contentContainer.css( { marginTop:  -height + 'px' } );
+
+			this._trigger( 'scroll', event );
 		},
 
 		destroy : function(){
