c0583b9ac1f4c730294a579af345e8d751fd6971
Richard Willis <willis.rh@gmail.com>
2010-06-02 19:51:34 +0100

    render: added functionality to search and replace a specified allowed list of element attibutes

--- render/js/jquery.render.js
+++ render/js/jquery.render.js
@@ -7,17 +7,23 @@
 
 (function( $ ) {
 
-	$.fn.render = function(data){
+	$.fn.render = function(data, options){
 
 		data = data || {};
 
-		function walkTextNodes(textNodeCallback){
+		options = $.extend({
+			attrs: 'href,class,src,style'.split(',')
+		}, options);
+
+		function walkTextNodes(textNodeCallback, elementNodeCallback){
 
 			( this.nodeType === 3 && textNodeCallback ) && textNodeCallback.call( this );
 
+			( this.nodeType === 1 && elementNodeCallback ) && elementNodeCallback.call( this );
+
 			$.each(this.childNodes, function(){
 
-				walkTextNodes.call(this, textNodeCallback);
+				walkTextNodes.call(this, textNodeCallback, elementNodeCallback);
 			});
 		}
 
@@ -27,30 +33,57 @@
 		}
 
 		function replaceHTML( val ){
-
 				
 			$( this ).after( val ).remove(); 
 		}
 
+		function replace( val ) {
+
+			while( match = /\{(.*?)\}/g.exec( val ) ) {
+
+				if ( data[match[1]] ) { 
+			
+					func = /<[^>]+>/.test( data[match[1]] ) ? replaceHTML : replaceText,
+
+					val = val.replace( new RegExp('\\$\{' + match[1] + '\}', 'g'), data[match[1]] );
+
+					func.call( this, val );
+				}
+			}
+		}
+
 		return this.each(function(){
 
-			walkTextNodes.call(this, function(){
-
+			walkTextNodes.call(this, 
+			function(){
+				// textnode callback
+	
 				var nodeValue = this.nodeValue.replace( new RegExp('\\n', 'g'), '' );
 
-				while( match = /\{(.*?)\}/g.exec( nodeValue ) ) {
+				replace.call( this, nodeValue );
 
-					if ( data[match[1]] ) { 
+			},
+			function(){
+				// elementnode callback
 
-						var 
-					
-						func = /<[^>]+>/.test( data[match[1]] ) ? replaceHTML : replaceText,
+				var el = this;
 
-						val = this.nodeValue.replace( new RegExp('\\$\{' + match[1] + '\}', 'g'), data[match[1]] );
+				$.each(options.attrs, function(){
 
-						func.call( this, val );
+					var attr = el[this];
+
+					if ( attr ) {
+
+						if ( typeof attr == 'string' ){
+
+							var text = document.createTextNode( attr );	
+
+							replace.call( text, unescape(attr) );
+
+							el[this] = text.nodeValue;
+						}
 					}
-				}
+				});
 			});
 		});
 	}; 
