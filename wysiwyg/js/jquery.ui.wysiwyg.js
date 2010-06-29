a73e5108faafc84b2008950d408fde82ef81dd5d
Richard Willis <willis.rh@gmail.com>
2010-06-29 20:22:16 +0100

    wysiwyg: added wysiwyg icons

--- wysiwyg/js/jquery.ui.wysiwyg.js
+++ wysiwyg/js/jquery.ui.wysiwyg.js
@@ -17,6 +17,8 @@
 			
 			this.elements = {};
 
+			this.element.addClass( this.widgetBaseClass + '-editor' );
+
 			this._createToolbar();
 
 			this._edit();
@@ -31,7 +33,6 @@
 			$.extend(this.options.toolbar, {
 
 				buttons: {
-
 					'Bold': function(){
 
 						self._exec('bold');
@@ -39,21 +40,65 @@
 					'Italic': function(){
 
 						self._exec('italic');
+					},
+					'Underline': function(){
+						
+						self._exec('underline');
+					},
+					'Ul': function(){
+						
+						self._insert('ul');
+					},
+					'Ol': function(){
+						
+						self._insert('ol');
+					},
+					'Link': function(){
+						
+						self._exec('link');
+					},
+					'Unlink': function(){
+						
+						self._exec('unlink');
+					},
+					'Indent': function(){
+						
+						self._exec('indent');
+					},
+					'Outdent': function(){
+						
+						self._exec('outdent');
 					}
 				}
 			});
 
-			this.elements.toolbar = $('<div></div>').addClass( this.widgetBaseClass + '-toolbar');
+			this.elements.toolbar = 
+				$('<ul></ul>').addClass( this.widgetBaseClass + '-toolbar ui-helper-reset ui-helper-clearfix');
 
 			$.each( this.options.toolbar.buttons, function( index, val ){
 
-				$('<a href="#">' + index + '</a>')
+				$('<li></li>')
+				.addClass( 'ui-state-default ui-corner-all' )
+				.attr( 'title', index )
 				.click(function(){
+
 					val.apply();
-				}).appendTo( self.elements.toolbar );
+
+					return false;
+
+				})
+				.append( '<span class="ui-icon ui-icon-toolbar-' + index.toLowerCase() + '"></span>' )
+				.appendTo( self.elements.toolbar );
 			});
 			
 			this.element.before( this.elements.toolbar );
+
+			this.elements.toolbar.find('li')
+			.bind('mouseenter mouseleave', function(){
+				$( this ).toggleClass('ui-state-hover');
+			});
+
+			this.elements.toolbar.disableSelection();
 		},
 
 		_exec : function( name ){
