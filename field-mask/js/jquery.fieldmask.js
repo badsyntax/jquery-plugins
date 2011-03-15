a5ffa9c3a1fc63b218a8ff44562df131ef336a89
Richard <willis.rh@gmail.com>
2011-03-15 13:02:26 +0000

    better variable/function names and comments

--- field-mask/js/jquery.fieldmask.js
+++ field-mask/js/jquery.fieldmask.js
@@ -7,8 +7,19 @@
 
 		var helper = $('<div />');
 
-		function type(ftype){
-			var field = $(this).after(helper).remove().attr('type', ftype).data('password', (ftype === 'text'));
+		function changeType(type){
+			/*
+			1- Add a helper div after the field
+			2- Remove the field from the DOM
+			3- Change the field type
+			4- Add the field back into the DOM, and remove the helper div
+			*/
+			var field = $(this)
+				.after(helper)
+				.remove()
+				.data('password', (this.type === 'password'))
+				.attr('type', type);
+
 			helper.after(field).remove();
 			return field;
 		}
@@ -18,14 +29,21 @@
 			if (this.value === this.title) {
 				this.value = '';
 			}
-			if ($(this).data('password')) {
-				type.call(this, 'password').focus();
+			// If the text field was originally of type 'password',
+			// then convert it back to a password field to mask the user input.
+			if (this.type === 'text' && $(this).data('password')) {
+				var field = changeType.call(this, 'password');
+				setTimeout(function(){
+					field[0].focus();
+				});
 			}
-   		})
+		})
 		.live('blur.mask', function(event) {
 			if (!this.value.length)	{
+				// If it's a password field, then we need to convert it to 
+				// type 'text' so can display the placeholder value.
 				if (this.type === 'password') {
-					type.call(this, 'text');
+					changeType.call(this, 'text');
 				}
 				this.value = this.title;
 			}
@@ -35,7 +53,7 @@
 		if (config.resetOnSubmit) {
 			this.each(function(){
 				var field = $(this);
-				field parents('form').submit(function(){
+				field.parents('form').submit(function(){
 					field[0].value = field[0].value === field[0].title ? '' : field[0].value;
 				});
 			});
